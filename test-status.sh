c5afe1ef846dd4da0f179d8a5c7ba5bad84196b4
dcelemeno <dmcoughlin2@gmail.com>
2018-07-29 11:31:55 -0400

    further editing of shell scripts and etc

--- test-status.sh
+++ test-status.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 cd ~/projects/smartlift-node-monitor
-git status
+#git status -sb
 
+#git diff origin/working-fixing..working-fixing^^
 
+UPSTREAM=${1:-'@{u}'} 
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
 
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Update(s) Pending"
+elif [ $REMOTE = $BASE ]; then
+    echo "incorrectly synced cache - contact system administrator"
+else
+    echo "Issue occurred - contact system administrator"
+fi
--- update-app.sh
+++ update-app.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# rebuild smartlift node monitor
+# pull...
 # cd ~/projects/smartlift-node-monitor
-# npm install
-# typings install
-# ./node_modules/.bin/electron-rebuild
-# npm run build
-# electron app ws://zuik.dnsalias.net:8095
-
+# git pull
+# then rebuild...
