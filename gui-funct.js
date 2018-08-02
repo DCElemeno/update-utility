5886da5ae4494cc93c7badfc44006526c629ebdb
Don Coughlin <DCElemeno@gmail.com>
2018-08-02 12:48:22 -0400

    refactor the individual functions

--- gui-funct.js
+++ gui-funct.js
@@ -2,87 +2,32 @@
 function appendToDroidOutput(msg) { getDroidOutput().value += msg; };
 function setStatus(msg)           { getStatus().innerHTML = msg; };
 
-// Function to check git status
-function checkForUpdates() {
-    const process = require('child_process');   // The power of Node.JS
-
-    // var ls = process.spawn('ls', ['-l']);
-    var ls = process.spawn('./test-status.sh');
-    // var ls = process.spawn('./shell-scripts/check-for-updates.sh');
+// Function to run shell script
+function runScript(filepath) {
+    const process = require('child_process');
+    var ls = process.spawn(filepath);
 
     ls.stdout.on('data', function (data) {
-      // console.log('stdout: <' + data+'> ');
-          // appendToDroidOutput(data);
-      appendToDroidOutput(data+'\n');
+        // console.log('stdout: <' + data+'> ');
+        appendToDroidOutput(data+'\n');
     });
 
     ls.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
+        console.log('stderr: ' + data);
     });
 
     ls.on('close', function (code) {
-      // console.log('child process exited with code ' + code);
-          if (code == 0)
-         setStatus('Finished checking for updates.');
-          else
-         setStatus('Error : code ' + code);
-          getDroidOutput().style.background = "DarkGray";
+        // console.log('child process exited with code ' + code);
+        if (code == 0) {
+            setStatus('Finished checking for updates.');
+        } else {
+            setStatus('Error : code ' + code);
+            getDroidOutput().style.background = "DarkGray";
+        }
     });
-};
+}
 
-// Function to update repo
-function updateApp() {
-    const process = require('child_process');   // The power of Node.JS
-
-    // var ls = process.spawn('ls', ['-l']);
-    var ls = process.spawn('./update-app.sh');
-    // var ls = process.spawn('./shell-scripts/check-for-updates.sh');
-
-    ls.stdout.on('data', function (data) {
-      // console.log('stdout: <' + data+'> ');
-          // appendToDroidOutput(data);
-      appendToDroidOutput(data+'\n');
-    });
-
-    ls.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
-    });
-
-    ls.on('close', function (code) {
-      // console.log('child process exited with code ' + code);
-          if (code == 0)
-         setStatus('Finished checking for updates.');
-          else
-         setStatus('Error : code ' + code);
-          getDroidOutput().style.background = "DarkGray";
-    });
-};
-
-
-// Function to update repo
-function backToApp() {
-    const process = require('child_process');   // The power of Node.JS
-
-    // var ls = process.spawn('ls', ['-l']);
-    var ls = process.spawn('./toApp.sh');
-    // var ls = process.spawn('./shell-scripts/check-for-updates.sh');
-
-    ls.stdout.on('data', function (data) {
-      // console.log('stdout: <' + data+'> ');
-          // appendToDroidOutput(data);
-      appendToDroidOutput(data+'\n');
-    });
-
-    ls.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
-    });
-
-    ls.on('close', function (code) {
-      // console.log('child process exited with code ' + code);
-          if (code == 0)
-         setStatus('Finished checking for updates.');
-          else
-         setStatus('Error : code ' + code);
-          getDroidOutput().style.background = "DarkGray";
-    });
-};
+// Functions to call various scripts
+function checkForUpdates() { runScript('./scripts/test-status.sh'); } // check git status
+function updateApp() { runScript('./scripts/update-app.sh'); } // git pull and update nodemon
+function backToApp() { runScript('./scripts/toApp.sh'); } // switch awesomewm workspace to nodemon
