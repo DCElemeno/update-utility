36e762dde7d60bacd91f8aefa264cf336f9334b4
Don Coughlin <DCElemeno@gmail.com>
2018-08-02 12:14:22 -0400

    update the pull script, still uses git credentials

--- update-app.sh
+++ update-app.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # pull...
-# cd ~/projects/smartlift-node-monitor
-# git pull
-# then rebuild...
+cd ~/projects/smartlift-node-monitor
+git pull
+
+# rebuild
+npm install
+typings install
+./node_modules/.bin/electron-rebuild 
+npm run build
+npm run electron
